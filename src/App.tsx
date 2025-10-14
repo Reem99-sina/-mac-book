ef0ec763ca1ddc7750bdad5fced34667d95a2f98
Reem99-sina <reemsina2@gmail.com>
2025-10-14 17:02:10 +0300

    inital project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./components/navbar";
+
+interface Props {}
+
+function App(props: Props) {
+  const {} = props;
+
+  return (
+    <main>
+      <Navbar/>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { navLinks } from "../constants";
+
+interface Props {}
+
+function Navbar(props: Props) {
+  const {} = props;
+
+  return (
+    <header>
+      <nav>
+        <img src="/logo.svg" alt="Logo" />
+        <ul>
+          {navLinks.map((item) => (
+            <li key={item.label}>
+              <a href={item.label} className="text-white hover:underline">
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className="flex-center gap-3">
+          <button>
+            <img src="/search.svg" alt="Search" />
+          </button>
+            <button>
+            <img src="/cart.svg" alt="cart" />
+          </button>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
